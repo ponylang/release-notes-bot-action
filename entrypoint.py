6d311b0594cc5dcb5faa1bc67df7070a65ed631f
Sean T. Allen <sean@seantallen.com>
2020-05-25 11:05:24 -0400

    proper commenting

--- entrypoint.py
+++ entrypoint.py
@@ -83,5 +83,5 @@ git.commit('-m', "Updating release notes for PR #" + str(pr_id)+ " [skip-ci]")
 
 print(INFO + "Pushing updated release notes." + ENDC)
 push_to = "https://" + os.environ['API_CREDENTIALS'] + "@github.com/" + repo_name + ".git"
-//git.push(push_to, 'master')
+#git.push(push_to, 'master')
 os.system("/usr/bin/git " + push_to + " master")
