a28a6596d0675c430b1d79c24dfd4a8ecef71566
Sean T Allen <sean@seantallen.com>
2020-06-21 10:26:54 -0400

    Initial work

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# release-notes-bot-action
-:horse: Bot to update release notes with new entries
+# Release-notes-bot action
+
+Automatically adds a new release notes entry to the upcoming release notes file once a PR is merged if a corresponding release notes addition file is present.
+
+A repo must have a `.release-notes` directory and a `.release-notes/next-release.md` file for the action to work properly.
+
+## Example workflow
+
+```yml
+name: Release Notes Bot
+
+on:
+  push:
+    branches:
+      - master
+    paths-ignore:
+      - .release-notes/next-release.md
+
+jobs:
+  release-notes-bot:
+    runs-on: ubuntu-latest
+    name: Update release notes
+    steps:
+      - name: Update
+        uses: ponylang/release-notes-bot-action@initial
+        with:
+          git_user_name: "Ponylang Main Bot"
+          git_user_email: "ponylang.main@gmail.com"
+        env:
+          API_CREDENTIALS: ${{ secrets.GITHUB_TOKEN }}
+```
+
+Note, you do not need to create `GITHUB_TOKEN`. It is already provided by GitHub. You merely need to make it available to the release-notes-bot action.
---
+++ entrypoint.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import git,json,os,sys
+from github import Github
+
+ENDC   = '\033[0m'
+ERROR  = '\033[31m'
+INFO   = '\033[34m'
+NOTICE = '\033[33m'
+
+if not 'API_CREDENTIALS' in os.environ:
+  print(ERROR + "API_CREDENTIALS needs to be set in env. Exiting." + ENDC)
+  sys.exit(1)
+
+# login
+github = Github(os.environ['API_CREDENTIALS'])
+
+# get json data for our event
+event_data = json.load(open(os.environ['GITHUB_EVENT_PATH'], 'r'))
+
+# grab info needed to find PR
+sha = event_data['head_commit']['id']
+repo_name = event_data['repository']['full_name']
+
+# find associated PR (if any)
+print(INFO + "Finding PR associated with " + sha + " in " + repo_name + ENDC)
+query = "q=is:merged+sha:" + sha + "+repo:" + repo_name
+print(INFO + "Query: " + query + ENDC)
+results = github.search_issues(query='is:merged', sha=sha, repo=repo_name)
+
+if results.totalCount == 0:
+  print(NOTICE + "No merged PR associated with " + sha + ". Exiting.")
+  sys.exit(0)
+
+pr_id = results[0].number
+
+# find associated release notes file
+release_notes_file = None
+repo = github.get_repo(repo_name)
+for commit in event_data['commits']:
+    c = repo.get_commit(sha=commit['id'])
+    for f in c.files:
+      if f.filename.startswith('.release-notes/'):
+        if not f.filename.endswith('next-release.md'):
+          release_notes_file = f.filename
+
+# if no release notes file, exit
+if release_notes_file is None:
+  print(NOTICE + "No release notes fie found in commits. Exiting." + ENDC)
+  sys.exit(0)
+
+print(INFO + "Cloning repo." + ENDC)
+clone_from = "https://" + os.environ['GITHUB_ACTOR'] + ":" + os.environ['API_CREDENTIALS'] + "@github.com/" + repo_name
+git = git.Repo.clone_from(clone_from, '.').git
+
+print(INFO + "Setting up git configuration." + ENDC)
+git.config('--global', 'user.name', os.environ['INPUT_GIT_USER_NAME'])
+git.config('--global', 'user.email', os.environ['INPUT_GIT_USER_EMAIL'])
+
+release_notes = open(release_notes_file, 'r').read().rstrip() + '\n\n'
+next_release_notes = open('.release-notes/next-release.md', 'a+')
+next_release_notes.write(release_notes)
+next_release_notes.close()
+
+print(INFO + "Adding git changes." + ENDC)
+git.rm(release_notes_file)
+git.add('.release-notes/next-release.md')
+git.commit('-m', "Updating release notes for PR #" + str(pr_id)+ " [skip-ci]")
+
+print(INFO + "Pushing updated release notes." + ENDC)
+git.push()
