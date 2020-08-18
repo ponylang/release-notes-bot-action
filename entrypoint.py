1cd26c371da1072494f7531a22d124f4c34dab32
Sean T. Allen <sean@seantallen.com>
2020-08-18 17:44:49 -0400

    entrypoint.py formatting

--- entrypoint.py
+++ entrypoint.py
@@ -11,8 +11,8 @@ INFO   = '\033[34m'
 NOTICE = '\033[33m'
 
 if not 'API_CREDENTIALS' in os.environ:
-  print(ERROR + "API_CREDENTIALS needs to be set in env. Exiting." + ENDC)
-  sys.exit(1)
+    print(ERROR + "API_CREDENTIALS needs to be set in env. Exiting." + ENDC)
+    sys.exit(1)
 
 # login
 github = Github(os.environ['API_CREDENTIALS'])
@@ -31,8 +31,8 @@ print(INFO + "Query: " + query + ENDC)
 results = github.search_issues(query='is:merged', sha=sha, repo=repo_name)
 
 if results.totalCount == 0:
-  print(NOTICE + "No merged PR associated with " + sha + ". Exiting.")
-  sys.exit(0)
+    print(NOTICE + "No merged PR associated with " + sha + ". Exiting.")
+    sys.exit(0)
 
 pr_id = results[0].number
 print(INFO + "PR found " + str(pr_id) + ENDC)
@@ -41,20 +41,20 @@ print(INFO + "PR found " + str(pr_id) + ENDC)
 release_notes_files = []
 repo = github.get_repo(repo_name)
 for commit in event_data['commits']:
-  print(INFO + "Examining files in commit " + str(commit['id']) + ENDC)
-  c = repo.get_commit(sha=commit['id'])
-  for f in c.files:
-    if f.status != "added":
-      continue
-    print(INFO + "Found file " + f.filename + ENDC)
-    if f.filename.startswith('.release-notes/'):
-      if not f.filename.endswith('next-release.md'):
-        release_notes_files.append(f.filename)
+    print(INFO + "Examining files in commit " + str(commit['id']) + ENDC)
+    c = repo.get_commit(sha=commit['id'])
+    for f in c.files:
+        if f.status != "added":
+            continue
+        print(INFO + "Found file " + f.filename + ENDC)
+        if f.filename.startswith('.release-notes/'):
+            if not f.filename.endswith('next-release.md'):
+                release_notes_files.append(f.filename)
 
 # if no release notes files, exit
 if not release_notes_files:
-  print(NOTICE + "No release notes file found in commits. Exiting." + ENDC)
-  sys.exit(0)
+    print(NOTICE + "No release notes file found in commits. Exiting." + ENDC)
+    sys.exit(0)
 
 print(INFO + "Cloning repo." + ENDC)
 pull_request = repo.get_pull(pr_id)
@@ -71,43 +71,43 @@ git.config('--global', 'user.email', os.environ['INPUT_GIT_USER_EMAIL'])
 # if it didn't delete the release notes file(s) and exit.
 found_changelog_label = False
 for l in pull_request.labels:
-  print(INFO + "PR had label: " + l.name + ENDC)
-  if l.name in changelog_labels:
-    found_changelog_label = True
-    break
+    print(INFO + "PR had label: " + l.name + ENDC)
+    if l.name in changelog_labels:
+        found_changelog_label = True
+        break
 
 if found_changelog_label:
-  print(NOTICE + "Processing release notes." + ENDC)
-  release_notes = ""
-  for rnf in release_notes_files:
-    release_notes += open(rnf, 'r').read().rstrip() + '\n\n'
-  next_release_notes = open('.release-notes/next-release.md', 'a+')
-  next_release_notes.write(release_notes)
-  next_release_notes.close()
+    print(NOTICE + "Processing release notes." + ENDC)
+    release_notes = ""
+    for rnf in release_notes_files:
+        release_notes += open(rnf, 'r').read().rstrip() + '\n\n'
+    next_release_notes = open('.release-notes/next-release.md', 'a+')
+    next_release_notes.write(release_notes)
+    next_release_notes.close()
 
-  print(INFO + "Adding git changes." + ENDC)
-  for rnf in release_notes_files:
-    git.rm(rnf)
-  git.add('.release-notes/next-release.md')
-  git.commit('-m', "Updates release notes for PR #" + str(pr_id))
+    print(INFO + "Adding git changes." + ENDC)
+    for rnf in release_notes_files:
+        git.rm(rnf)
+    git.add('.release-notes/next-release.md')
+    git.commit('-m', "Updates release notes for PR #" + str(pr_id))
 else:
-  print(NOTICE + "Found release notes but no changelog label." + ENDC)
-  for rnf in release_notes_files:
-    git.rm(rnf)
-  git.commit('-m', "Removes release notes from changelog labelless PR #"
+    print(NOTICE + "Found release notes but no changelog label." + ENDC)
+    for rnf in release_notes_files:
+        git.rm(rnf)
+    git.commit('-m', "Removes release notes from changelog labelless PR #"
     + str(pr_id))
 
 print(INFO + "Pushing changes." + ENDC)
 push_failures = 0
-while(True):
-  try:
-    git.push()
-    break
-  except:
-    push_failures += 1
-    if (push_failures <= 5):
-      print(NOTICE + "Failed to push. Going to pull and try again." + ENDC)
-      git.pull()
-    else:
-      print(ERROR + "Failed to push again. Giving up." + ENDC)
-      raise
+while True:
+    try:
+        git.push()
+        break
+    except:
+        push_failures += 1
+        if (push_failures <= 5):
+            print(NOTICE + "Failed to push. Going to pull and try again." + ENDC)
+        git.pull()
+        else:
+            print(ERROR + "Failed to push again. Giving up." + ENDC)
+            raise
